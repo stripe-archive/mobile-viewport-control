818a3e8fb7d3ae62d66bff3e77dae7b80c54d274
Shaun Williams <shaun@stripe.com>
2016-04-22 16:29:33 -0500

    make width configurable from common test css

--- test.js
+++ test.js
@@ -1,11 +1,15 @@
 var globalTest = {
   success: true,
+  totalPass: 0,
   cases: [],
   waitLength: 100,
 };
 
 function assert(test) {
   test.success = test.actual === test.expected;
+  if (test.success) {
+    globalTest.totalPass++;
+  }
 
   globalTest.success &= test.success;
   globalTest.cases.push(test);
@@ -28,4 +32,18 @@ function assert(test) {
       "</div>"
     );
   }
+}
+
+function testSequence(funcs) {
+  if (funcs.length === 0) {
+    resultDiv.innerHTML += (
+      (globalTest.success ? "SUCCESS" : "FAILED") +
+      " (" + globalTest.totalPass + "/" + globalTest.cases.length + " passed)"
+    );
+    return;
+  }
+  setTimeout(function(){
+    funcs[0]();
+    testSequence(funcs.slice(1));
+  }, globalTest.waitLength);
 }
