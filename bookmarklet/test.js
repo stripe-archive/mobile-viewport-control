07a614d818f3d6ddb339bb799b5b06221050f3c2
Shaun Williams <shaun@stripe.com>
2016-04-28 13:53:43 -0500

    print viewport in bookmarklet correctly

--- bookmarklet/test.js
+++ bookmarklet/test.js
@@ -1,5 +1,6 @@
 (function(){
-  alert("About to freeze viewport for 5 seconds... Viewport is: "+mobileViewportControl.getInitialViewport())
+  var viewport = JSON.stringify(mobileViewportControl.getInitialViewport());
+  alert("About to freeze viewport for 5 seconds... Viewport is: "+viewport)
   mobileViewportControl.freeze(1, function(){
     setTimeout(mobileViewportControl.thaw, 5000);
   });
