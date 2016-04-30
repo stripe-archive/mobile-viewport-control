f174080eb60932360f284fc65cfe1b8bdce4a809
Shaun Williams <shaun@stripe.com>
2016-04-29 19:27:16 -0500

    update bookmarklet to display an isolated element describing expected behavior and a button to restore the view when ready

--- test/bookmarklet.js
+++ test/bookmarklet.js
@@ -1,23 +1,36 @@
 (function(){
-  function test(viewport) {
-    var isolated = document.createElement('div');
-    isolated.id = 'myIsolatedElement';
-    isolated.appendChild(document.createTextNode('I should be the only visible thing, but everything will be restored soon...'))
-    document.body.appendChild(isolated);
-
-    var initial = JSON.stringify(viewport.getInitialViewport());
-    alert("About to freeze viewport for 5 seconds... Viewport is: "+initial);
-    viewport.freeze(1, isolated.id, function(){
-      setTimeout(function(){
-        document.body.removeChild(isolated);
-        viewport.thaw();
-      }, 5000);
-    });
-  }
   var viewport = window.mobileViewportControl;
   if (viewport) {
-    test(viewport);
+    test();
   } else {
-    require(['mobile-viewport-control'], test);
+    require(['mobile-viewport-control'], function(module) {
+      viewport = module;
+      test();
+    });
+  }
+
+  function test() {
+    var isolated = createIsolatedElement();
+    var initial = JSON.stringify(viewport.getInitialViewport());
+    alert("About to freeze viewport on a custom element... Viewport is: "+initial);
+    viewport.freeze(1, isolated.id);
+  }
+
+  function createIsolatedElement() {
+    var isolated = document.createElement('div');
+    isolated.style.cssText = 'color: #000; text-align: left; font-size: 12px; padding: 20px;';
+    isolated.id = 'myIsolatedElement';
+    isolated.innerHTML = 'I should be the only visible thing, and you should not be able to pinch zoom or scroll.<br>';
+
+    var btn = document.createElement('button');
+    btn.innerHTML = 'Click me to restore!';
+    btn.onclick = function() {
+      document.body.removeChild(isolated);
+      viewport.thaw();
+    }
+    isolated.appendChild(btn);
+
+    document.body.appendChild(isolated);
+    return isolated;
   }
 }());
