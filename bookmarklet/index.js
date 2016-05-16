11c739db6262fa893f1d7de87ff003be09d55940
Shaun Williams <shaun@stripe.com>
2016-05-16 11:25:21 -0500

    bookmarklet: dont use test button, make whole area clickable to help automation

--- bookmarklet/index.js
+++ bookmarklet/index.js
@@ -463,15 +463,11 @@ return {
     var isolated = document.createElement('div');
     isolated.style.cssText = 'color: #000; text-align: left; font-size: 12px; padding: 20px;';
     isolated.id = 'myIsolatedElement';
-    isolated.innerHTML = 'I should be the only visible thing, and you should not be able to pinch zoom or scroll.<br>';
-
-    var btn = document.createElement('button');
-    btn.innerHTML = 'Click me to restore!';
-    btn.onclick = function() {
+    isolated.onclick = function() {
       document.body.removeChild(isolated);
       viewport.thaw();
     }
-    isolated.appendChild(btn);
+    isolated.innerHTML = 'I should be the only visible thing, and you should not be able to pinch zoom or scroll.<br>CLICK TO RESTORE.';
 
     document.body.appendChild(isolated);
     return isolated;
